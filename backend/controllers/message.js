8b7d9c9b0096ab065f2bce506c579d5e8df2ad69
suraj <surajsah1215@gmail.com>
2023-07-23 17:11:49 +0530

    Optimising the Chat App

--- backend/controllers/message.js
+++ backend/controllers/message.js
@@ -5,17 +5,17 @@ exports.messageSent = async(req,res,next)=>{
     try{
         const message = req.body.mess
         const messagExist = await Message.findOne({where:{userId:req.user.id}})
-        if(messagExist){
-            const response = await Message.update({message:message},{where:{userId:req.user.id}})
-            return res.status(200).json({message:'updated',res:response})
-        }
-        else{
+        // if(messagExist){
+        //     const response = await Message.update({message:message},{where:{userId:req.user.id}})
+        //     return res.status(200).json({message:'updated',res:response})
+        // }
+        // else{
             const response = await Message.create({
             message:message,
             userId : req.user.id
             })
             return res.status(200).json({res:response})
-        }
+        // }
         
     }
     catch(err){
@@ -28,12 +28,28 @@ exports.messageSent = async(req,res,next)=>{
 
 exports.getMessages = async(req,res,next)=>{
     try{
-        const allMessages = await Message.findAll({attributes:['message']})
-        return res.status(200).json({message:allMessages})
+        const messages = await Message.findAll({
+            include: [{ model: User, attributes: ['name'] }],
+          });
+          const messagesWithUserName = messages.map((message) => {
+            return {
+              id: message.id,
+              text: message.message,
+              userName: message.user.name,
+            };
+          });   
+        if (res && typeof res === 'object' && res.json) {
+            return res.json(messagesWithUserName);
+          } else {
+            console.error('Invalid response object:', res);
+            return res.send('Invalid response object');
+          } 
     }
     catch(err){
-        res.status(404).res.json({message:'cannot find messages'})
-    }
+    
+    return res.status(500).json({ error: 'Server error' });
+   
+  }
 
     
 }
